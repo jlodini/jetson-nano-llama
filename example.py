061d3b8e8b4e2052a6d5cb2b19b456ee1bd0af20
chuanli11 <cl.chuanli@gmail.com>
2023-03-11 03:29:13 +0000

    Support mpirun

--- example.py
+++ example.py
@@ -17,11 +17,21 @@ from llama import ModelArgs, Transformer, Tokenizer, LLaMA
 
 
 def setup_model_parallel(seed: int) -> Tuple[int, int]:
-    local_rank = int(os.environ.get("LOCAL_RANK", -1))
-    world_rank = int(os.environ.get("RANK", -1))
-    world_size = int(os.environ.get("WORLD_SIZE", -1))
+    if 'LOCAL_RANK' in os.environ:
+        # Environment variables set by torch.distributed.launch or torchrun
+        local_rank = int(os.environ['LOCAL_RANK'])
+        world_size = int(os.environ['WORLD_SIZE'])
+        world_rank = int(os.environ['RANK'])
+    elif 'OMPI_COMM_WORLD_LOCAL_RANK' in os.environ:
+        # Environment variables set by mpirun
+        local_rank = int(os.environ['OMPI_COMM_WORLD_LOCAL_RANK'])
+        world_size = int(os.environ['OMPI_COMM_WORLD_SIZE'])
+        world_rank = int(os.environ['OMPI_COMM_WORLD_RANK'])
+    else:
+        import sys
+        sys.exit("Can't find the evironment variables for local rank")
 
-    torch.distributed.init_process_group("nccl")
+    torch.distributed.init_process_group(backend="nccl", rank=world_rank, world_size=world_size)
     initialize_model_parallel(world_size)
     torch.cuda.set_device(local_rank)
 
@@ -78,7 +88,7 @@ def main(
     max_gen_len: int = 256,
     max_batch_size: int = 32,
     seed: int = 1,
-    count: int = 5,
+    count: int = 1,
 ):
     local_rank, world_rank, world_size = setup_model_parallel(seed)
     if local_rank > 0:
