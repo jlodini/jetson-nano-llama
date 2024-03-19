c7a06943256eba296f9c144b6b3d061f21a68241
John Lodini <john@unplatform.social>
2024-03-18 20:39:08 -0500

    Update setup_nodes.sh

--- deployment/setup_nodes.sh
+++ deployment/setup_nodes.sh
@@ -20,7 +20,7 @@ done
 
 echo "Let the head node ssh into the all nodes at least once so in the future it won't ask about fingerprint ------------------------------"
 for IP in ${ALL_IP[*]}; do
-sshpass -f password ssh -t jetson@$HEAD_IP "echo exit | xargs ssh ubuntu@$IP"
+sshpass -f password ssh -t jetson@$HEAD_IP "echo exit | xargs sshpass -f password ssh jetson@$IP"
 done
 
 echo "Set up NFS ------------------------------"
@@ -30,13 +30,13 @@ done
 
 
 for IP in ${WORKER_IP[*]}; do
-    sshpass -f password ssh jetson@$IP "sudo mount ${NFS_IP}:/mnt/shares /home/ubuntu/shared"
+    sshpass -f password ssh jetson@$IP "sudo mount ${NFS_IP}:/mnt/shares /home/jetson/shared"
 done
 echo "NFS set up on the worker nodes"
 
 echo "Clone repos into NFS ------------------------------"
-sshpass -f password ssh jetson@$HEAD_IP "if [ ! -d /home/ubuntu/shared/llama ]; then git clone https://github.com/LambdaLabsML/llama.git /home/ubuntu/shared/llama; fi"
-sshpass -f password ssh jetson@$HEAD_IP "if [ ! -d /home/ubuntu/shared/llama-dl ]; then git clone https://github.com/chuanli11/llama-dl.git /home/ubuntu/shared/llama-dl; fi"
+sshpass -f password ssh jetson@$HEAD_IP "if [ ! -d /home/jetson/shared/llama ]; then git clone https://github.com/LambdaLabsML/llama.git /home/jetson/shared/llama; fi"
+sshpass -f password ssh jetson@$HEAD_IP "if [ ! -d /home/jetson/shared/llama-dl ]; then git clone https://github.com/chuanli11/llama-dl.git /home/jetson/shared/llama-dl; fi"
 
 echo "Install LLAMA dependencies (asynchronously) ------------------------------"
 for IP in ${ALL_IP[*]}; do cat dependencies-install.sh | sshpass -f password ssh jetson${IP} & done
